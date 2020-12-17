d2359b1e897f2273d79ae21c05b8a8b83744cc07
Lewis Tian <taseikyo@gmail.com>
2020-12-17 21:06:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+> @Date    : 2020-01-17 19:20:00
+>
+> @Author  : Lewis Tian (taseikyo@gmail.com)
+>
+> @Link    : github.com/taseikyo
+
+![Sublime Markdown Helper](https://socialify.git.ci/taseikyo/sublime-markdown-helper/image?forks=1&issues=1&language=1&owner=1&pattern=Brick%20Wall&pulls=1&stargazers=1&theme=Light)
+
+A simple Sublime Text 3 plugin can help you write Markdown faster.
+
+I am new to the st3 plugin, and my motivation is to focus on text rather than Markdown syntax. My task is to write the text, and the rest of the work is done by the plugin, which converts the plain text into a markdown document.
+
+## preview
+
+![preview](images/preview.gif)
+
+## how to use
+
+Go to Preferences -> Browse Packages, and then either download and unzip this
+plugin into that directory, or:
+
+```Bash
+cd $ST3Packages
+git clone https://github.com/taseikyo/sublime-markdown-helper.git
+```
+
+### shortcuts
+
+| Keyboard shortcut |	Description  |
+|-------------------|----------------|
+|ctrl + 1-3         |h1 - h3         |
+|ctrl + 4	        |ul              |
+|ctrl + 5			|ol 			 |
+|ctrl + 6			|code (block codes)|
+|ctrl + 7			|latex formula (block)|
+
+**Note: Remember to select the entire line before you press the shortcuts.**
+
+## License
+
+Copyright (c) 2020 Lewis Tian. Licensed under the MIT license.
---
+++ markdown-helper.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Date    : 2020-01-17 ‏‎15:48:59
+# @Author  : Lewis Tian (taseikyo@gmail.com)
+# @Link    : github.com/taseikyo
+# @Version : python3.8
+
+"""
+A simple st3 plugin can help write markdown faster (maybe)
+Note: Remember to select the entire line and then press the shortcuts
+---
+shortcuts:
+    ctrl + 1-3: h1 - h3
+    ctrl + 4: ul
+    ctrl + 5: ol
+    ctrl + 6: code (block codes)
+    ctrl + 7: latex formula (block)
+"""
+
+
+import sublime
+import sublime_plugin
+
+
+class MarkdownHelperCommand(sublime_plugin.TextCommand):
+    def run(self, edit, md_type):
+        """
+        select function according $md_type
+        """
+        if md_type[0] == "h":
+            self.helper_hx(edit, int(md_type[1]))
+        elif md_type == "ul":
+            self.helper_ul(edit)
+        elif md_type == "ol":
+            self.helper_ol(edit)
+        elif md_type == "code":
+            self.helper_code(edit)
+        elif md_type == "lxf":
+            self.helper_lxf(edit)
+
+    def helper_h1(self, edit):
+        """
+        generate h1 title
+        ---
+        xxx => # xxx
+        """
+        # region consists many lines
+        # so we use `self.view.lines` to get all the lines (type: region)
+        for region in self.view.sel():
+            if not region.empty():
+                line = self.view.line(region)
+                # st3 uses py3.3, we cannot use f-strings ;(
+                text = "# " + self.view.substr(line).lstrip()
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(edit, line.begin(), text)
+
+    def helper_hx(self, edit, level):
+        """
+        generate hx title
+        ---
+        xxx => # xxx
+        xxx => ## xxx
+        ...
+        """
+        # region consists many lines
+        # so we use `self.view.lines` to get all the lines (type: region)
+        for region in self.view.sel():
+            if not region.empty():
+                subregions = self.view.lines(region)
+                new_text = []
+                for subregion in subregions:
+                    line = self.view.line(subregion)
+                    # st3 uses py3.3, we cannot use f-strings ;(
+                    text = "#" * level + " " + \
+                        self.view.substr(line).lstrip() + "\n"
+                    new_text.append(text)
+
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(
+                    edit, subregions[0].begin(), "".join(new_text))
+
+    def helper_ul(self, edit):
+        """
+        generate unordered list
+        ---
+        aaa
+        bbb
+        ccc
+        =>
+        - aaa
+        - bbb
+        - ccc
+        """
+        for region in self.view.sel():
+            if not region.empty():
+                subregions = self.view.lines(region)
+                new_text = []
+                for subregion in subregions:
+                    line = self.view.line(subregion)
+                    # st3 uses py3.3, we cannot use f-strings ;(
+                    text = "- " + self.view.substr(line).lstrip() + "\n"
+                    new_text.append(text)
+
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(
+                    edit, subregions[0].begin(), "".join(new_text))
+
+    def helper_ol(self, edit):
+        """
+        generate ordered list
+        ---
+        aaa
+        bbb
+        ccc
+        =>
+        1. aaa
+        2. bbb
+        3. ccc
+        """
+        for region in self.view.sel():
+            if not region.empty():
+                subregions = self.view.lines(region)
+                new_text = []
+                for index, subregion in enumerate(subregions):
+                    line = self.view.line(subregion)
+                    # st3 uses py3.3, we cannot use f-strings ;(
+                    text = (
+                        str(index + 1) + ". " +
+                        self.view.substr(line).lstrip() + "\n"
+                    )
+                    new_text.append(text)
+
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(
+                    edit, subregions[0].begin(), "".join(new_text))
+
+    def helper_code(self, edit):
+        """
+        generate code
+        ---
+        aaa
+        bbb
+        =>
+        ```
+        aaa
+        bbb
+        ```
+        """
+        for region in self.view.sel():
+            if not region.empty():
+                # Packages/Python/Python.tmLanguage
+                # stupid method, this will always return `Markdown`
+                syntax = self.view.settings().get(
+                    "syntax").split("/")[-1].split(".")[0]
+                line = self.view.line(region)
+                # st3 uses py3.3, we cannot use f-strings ;(
+                text = "```" + syntax + "\n" + \
+                    self.view.substr(line) + "\n```\n"
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(edit, line.begin(), text)
+
+    def helper_lxf(self, edit):
+        """
+        generate latex formula
+        ---
+        aaa
+        bbb
+        =>
+        $$
+        aaa
+        bbb
+        $$
+        """
+        for region in self.view.sel():
+            if not region.empty():
+                line = self.view.line(region)
+                # st3 uses py3.3, we cannot use f-strings ;(
+                text = "$$\n" + self.view.substr(line) + "\n$$\n"
+                # remove the old text and insert...
+                self.view.erase(edit, region)
+                self.view.insert(edit, line.begin(), text)
